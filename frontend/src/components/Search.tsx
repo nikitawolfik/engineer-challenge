7ff0eeb70de7bd6091b5ba1574012bf2bf6959c0
Nikita Neganov <nikita.neganov@arcadia.spb.ru>
2022-10-23 15:18:53 +0400

    fix: code style

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -7,21 +7,19 @@ export interface InputProps {
   name: string;
 }
 
-const Search = ({ value, onChange, label, name }: InputProps) => {
-  return (
-    <div className="flex flex-col mb-2">
-      <label className="text-xs ml-2 mb-1" htmlFor={`input-${name}`}>
-        {label}
-      </label>
-      <input
-        className="rounded-lg p-2 border-solid border-slate-50 border-2"
-        name={name}
-        value={value}
-        placeholder={label}
-        onChange={onChange}
-      />
-    </div>
-  );
-};
+const Search = ({ value, onChange, label, name }: InputProps) => (
+  <div className="flex flex-col mb-2">
+    <label className="text-xs ml-2 mb-1" htmlFor={`input-${name}`}>
+      {label}
+    </label>
+    <input
+      className="rounded-lg p-2 border-solid border-slate-50 border-2"
+      name={name}
+      value={value}
+      placeholder={label}
+      onChange={onChange}
+    />
+  </div>
+);
 
 export default Search;
